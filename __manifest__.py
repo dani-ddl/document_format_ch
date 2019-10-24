196d0ad78c4f21babe37883a9cf3387d00864395
dani-ddl <dani--ef-@hotmail.com>
2019-10-24 12:51:29 +0200

    initial version

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'document_format_ch',
+    'summary': """Formatos de documentos CH""",
+    'version': '12.0.1.0.0',
+    'description': """Formatos de documentos CH""",
+    'author': 'DDL',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.com',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'account',
+        'sale',
+        'web'
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/report_saleorder_document.xml',
+        'views/external_layout_clean.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+}
