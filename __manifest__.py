f6a85efaa2e1e4082bcf4650d5cccc7522e9241a
dani-ddl <dani--ef-@hotmail.com>
2019-10-28 14:01:32 +0100

    Añadida factura bluetooth

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,7 @@
         'views/report_invoice_document.xml',
         'views/report_delivery_document.xml',
         'views/report_saleorder_bluetooth.xml',
+        'views/report_invoice_bluetooth.xml',
     ],
     'demo': [],
     'installable': True,
